6cb7b9505cd819f10283489f317d54314c50238b
Vikrant Shinde <vikrant.shinde@brain2.com>
2025-05-30 11:01:19 +0530

    initial push

---
+++ MyFirstReactDotNetApp/myfirstreactdotnetapp.client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+    return (<div>
+        <HelloWorld />
+    </div>);
+}
+
+export default App;
---
+++ MyFirstReactDotNetApp/myfirstreactdotnetapp.client/src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, World, Hey Vikrant Shinde How Are you !</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
